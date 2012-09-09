68e8a7a623b89ff5a630860c0a8c8f569a3d57df
Eric Talevich <eric.talevich@gmail.com>
2012-09-09 15:11:27 -0400

    setup.py: correct dependencies for setuptools; minor cleanup

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+"""Bioinformatics utilities for sequence analysis."""
+
+from glob import glob
+from os.path import dirname
+
+DIR = (dirname(__file__) or '.') + '/'
+
 setup_args = {}
 
 try:
     from setuptools import setup
     from setuptools.extension import Extension
-    setup_args.update(install_requires=[
-        'Cython >= 0.11',
-        'numpy >= 1.5.1',
-        #'scipy >= 0.6',
-    ])
+    # Dependencies for easy_install:
+    setup_args.update(
+        install_requires=[
+            'Biopython >= 1.59',
+            'Cython >= 0.11',
+        ])
 except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
@@ -17,23 +25,21 @@ except ImportError:
 setup_args.update(
     name='biofrills',
     version='0.0.0-dev',
-    description='Bio scripts, parsers and utilities',
+    description=__doc__,
     author='Eric Talevich',
     author_email='etal@uga.edu',
-    url='http://etalog.blogspot.com/',
-    packages=['biofrills',
-             ],
+    url='http://github.com/etal/biofrills',
+    packages=['biofrills'],
+    #scripts=glob(DIR + 'scripts/*')
 )
 
 try:
-    from os.path import dirname
     from Cython.Distutils import build_ext
     setup_args.update(
         cmdclass={'build_ext': build_ext},
         ext_modules=[
             Extension('biofrills.cpairutils',
-                      [(dirname(__file__) or '.') +
-                       '/biofrills/cpairutils.pyx']),
+                      [DIR + 'biofrills/cpairutils.pyx']),
         ],
     )
 except ImportError:

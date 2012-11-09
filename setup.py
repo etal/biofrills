7d6fc8143bb9b126c33de68e1cdc0d3dd988dc67
Eric Talevich <eric.talevich@gmail.com>
2012-11-08 20:27:49 -0500

    setup.py: added metadata, set version to 0.1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python
 
-"""Bioinformatics utilities for sequence analysis."""
+"""Bioinformatics utilities for molecular sequence analysis."""
 
 from glob import glob
-from os.path import dirname
+from os.path import dirname, join
 
-DIR = (dirname(__file__) or '.') + '/'
+DIR = (dirname(__file__) or '.')
 
-setup_args = {}
+setup_args = dict(
+    name='biofrills',
+    version='0.1.0',
+    description=__doc__,
+    author='Eric Talevich',
+    author_email='eric.talevich@gmail.com',
+    url='http://github.com/etal/biofrills',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    packages=['biofrills'],
+    #scripts=glob(join(DIR, 'scripts', '*'))
+)
 
 try:
     from setuptools import setup
@@ -15,23 +33,12 @@ try:
     # Dependencies for easy_install:
     setup_args.update(
         install_requires=[
-            'Biopython >= 1.59',
+            'biopython >= 1.58',
             'Cython >= 0.11',
         ])
 except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
-
-setup_args.update(
-    name='biofrills',
-    version='0.0.0-dev',
-    description=__doc__,
-    author='Eric Talevich',
-    author_email='etal@uga.edu',
-    url='http://github.com/etal/biofrills',
-    packages=['biofrills'],
-    #scripts=glob(DIR + 'scripts/*')
-)
 
 try:
     from Cython.Distutils import build_ext
@@ -39,7 +46,7 @@ try:
         cmdclass={'build_ext': build_ext},
         ext_modules=[
             Extension('biofrills.cpairutils',
-                      [DIR + 'biofrills/cpairutils.pyx']),
+                      [join(DIR, 'biofrills', 'cpairutils.pyx')]),
         ],
     )
 except ImportError:

4fe901e7941d6f4403ee7f5352d79ee0cf727411
Eric Talevich <eric.talevich@gmail.com>
2012-11-08 20:23:50 -0500

    alnutils: made sequence_weights more rigorous
    Instead of taking the expected number of independent residue types in a column
    as the column height up to 20, I estimated the actual value for k=1..100 by
    repeated sampling and hard-coded those values in sequence_weights.
    
    Also loosened the requirement for "unique insert" from "nongap < 2" to
    "nongap < .2 * column height".

--- biofrills/alnutils.py
+++ biofrills/alnutils.py
@@ -8,7 +8,7 @@ from Bio.SeqRecord import SeqRecord
 from Bio import AlignIO
 
 
-def aa_frequencies(aln, gap_chars='-.', weights=None):
+def aa_frequencies(aln, weights=None, gap_chars='-.'):
     """Calculate the amino acid frequencies in a set of SeqRecords.
 
     Weights for each sequence in the alignment can be given as a list/tuple,
@@ -61,7 +61,7 @@ def remove_empty_cols(records):
                         letter_annotations=rec.letter_annotations)
 
 
-def sequence_weights(aln, scaling='none'):
+def sequence_weights(aln, scaling='none', gap_chars='-.'):
     """Weight aligned sequences to emphasize more divergent members.
 
     Returns a list of floating-point numbers between 0 and 1, corresponding to
@@ -73,6 +73,8 @@ def sequence_weights(aln, scaling='none'):
         - 'max1': Weights are all scaled so the max is 1.0.
         - 'avg1': Average (mean) weight is 1.0.
         - 'andy': Average (mean) weight is 0.5, ceiling is 1.0.
+        - 'none': Weights are scaled to sum to the effective number of
+                  independent sequences.
 
     Method: At each column position, award each different residue an equal
     share of the weight, and then divide that weight equally among the
@@ -81,9 +83,26 @@ def sequence_weights(aln, scaling='none'):
 
     See Henikoff & Henikoff (1994): Position-based sequence weights.
     """
+    # Probability is hard, let's estimate by sampling!
+    # Sample k from a population of 20 with replacement; how many unique k were
+    # chosen? Average of 10000 runs for k = 0..100
+    expectk = [0.0, 1.0, 1.953, 2.861, 3.705, 4.524, 5.304, 6.026, 6.724, 7.397,
+               8.04, 8.622, 9.191, 9.739, 10.264, 10.758, 11.194, 11.635,
+               12.049, 12.468, 12.806, 13.185, 13.539, 13.863, 14.177, 14.466,
+               14.737, 15.005, 15.245, 15.491, 15.681, 15.916, 16.12, 16.301,
+               16.485, 16.671, 16.831, 16.979, 17.151, 17.315, 17.427, 17.559,
+               17.68, 17.791, 17.914, 18.009, 18.113, 18.203, 18.298, 18.391,
+               18.46, 18.547, 18.617, 18.669, 18.77, 18.806, 18.858, 18.934,
+               18.978, 19.027, 19.085, 19.119, 19.169, 19.202, 19.256, 19.291,
+               19.311, 19.357, 19.399, 19.416, 19.456, 19.469, 19.5, 19.53,
+               19.553, 19.562, 19.602, 19.608, 19.629, 19.655, 19.67, 19.681,
+               19.7, 19.716, 19.724, 19.748, 19.758, 19.765, 19.782, 19.791,
+               19.799, 19.812, 19.82, 19.828, 19.844, 19.846, 19.858, 19.863,
+               19.862, 19.871, 19.882]
+
     def col_weight(column):
         """Represent the diversity at a position.
-        
+
         Award each different residue an equal share of the weight, and then
         divide that weight equally among the sequences sharing the same
         residue.
@@ -93,8 +112,9 @@ def sequence_weights(aln, scaling='none'):
         a score of 1/r to that sequence, whereas a residue represented in s
         sequences contributes a score of 1/rs to each of the s sequences.
         """
-        # Skip columns with all gaps or unique inserts
-        if len([c for c in column if c not in '-.']) < 2:
+        # Skip columns of all or mostly gaps (i.e. rare inserts)
+        min_nongap = max(2, .2*len(col))
+        if len([c for c in column if c not in gap_chars]) < min_nongap:
             return ([0] * len(column), 0)
         # Count the number of occurrences of each residue type
         # (Treat gaps as a separate, 21st character)
@@ -108,11 +128,12 @@ def sequence_weights(aln, scaling='none'):
         return (weights, n_residues)
 
     seq_weights = [0] * len(aln)
-    tot_nres = 0.0
+    tot_nres = 0.0  # Expected no. different types in independent seqs
     # Sum the contributions from each position along each sequence -> total weight
     for col in zip(*aln):
         wts, nres = col_weight(col)
-        tot_nres += nres
+        assert sum(wts) <= 20
+        tot_nres += expectk[nres] if nres < len(expectk) else 20
         for idx, wt in enumerate(wts):
             seq_weights[idx] += wt
     # Normalize w/ the given scaling criterion
